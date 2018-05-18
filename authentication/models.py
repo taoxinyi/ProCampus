7d53d455079a140a8666c3c63f89717f95d2c52a
taoxinyi <850804004@qq.com>
2018-05-18 10:30:41 +0800

    5.18 add chat user list

--- authentication/models.py
+++ authentication/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class MyUser(models.Model):
     IDENTITY_CHOICE = (
         ('T', u'老师'),
@@ -16,6 +17,7 @@ class MyUser(models.Model):
     identity = models.CharField(max_length=1, choices=IDENTITY_CHOICE)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     friend = models.ManyToManyField('self')
+    chat_room = models.ManyToManyField('chat.ChatRoomGroup')
 
     def __str__(self):
         return self.nickname
--- chat/consumers.py
+++ chat/consumers.py
@@ -1,21 +1,27 @@
 # chat/consumers.py
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import User
+
 from authentication.models import MyUser
 from chat.models import ChatRoomGroup, ChatHistory
 from channels.db import database_sync_to_async
 from datetime import datetime
 import json
 
+from forum.models import RequestReplyFriend
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.user = self.scope["user"]
-        self.myuser = await  self.get_myuser()
+        self.myuser = self.user.myuser
         self.username = self.get_username()
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        await self.save_group()
+
+        self.room_object = await self.save_group()
+        current_list = await self.return_to_current_list_and_add_to_db()
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -26,15 +32,36 @@ class ChatConsumer(AsyncWebsocketConsumer):
         s = await  self.get_history()
         await self.send(text_data=json.dumps({
             'message': s,
-            'type': 'clear'
+            'type': 'clear',
+            'current_list': current_list
         }))
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'client_enter',
+                'id': self.user.id,
+                'name': self.user.myuser.nickname,
+                'image': self.get_user_image_url(self.user)
+
+            }
+        )
 
     async def disconnect(self, close_code):
         # Leave room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'client_leave',
+                'id': self.user.id,
+                'name': self.user.myuser.nickname,
+                'image': self.get_user_image_url(self.user)
+
+            })
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+        await self.delete_from_db()
 
     # Receive message from WebSocket
     async def receive(self, text_data):
@@ -46,8 +73,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             {
                 'type': 'chat_message',
                 'message': message,
-                'author_id': self.myuser.id if self.user.is_authenticated else 0,
-                'image': str(self.myuser.photo) if self.user.is_authenticated else "",
+                'author_id': self.user.id if self.user.is_authenticated else 0,
                 'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             }
         )
@@ -59,26 +85,41 @@ class ChatConsumer(AsyncWebsocketConsumer):
         author_id = event['author_id']
         time = event['time']
         isAuthor = "0"
-        if self.user.is_authenticated and author_id == self.myuser.id:
+        if self.user.is_authenticated and author_id == self.user.id:
             isAuthor = "1"
-        if event['image'] == "":
-            if self.user.is_authenticated:
-                if self.myuser.identity == "T":
-                    image = '/static/image/default.png'
-                else:
-                    image = '/static/image/default1.png'
-            else:
-                image = '/static/image/default.png'
+        if author_id == 0:
+            image = '/static/image/default.png'
         else:
-            image = '/media/' + event['image']
+            image = self.get_user_image_url(User.objects.get(id=author_id))
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
+            'type': "chat_message",
             'message': message,
             'image': image,
             'isAuthor': isAuthor,
             'time': time,
-            'author': MyUser.objects.get(id=author_id).nickname if author_id > 0 else "Anonymous"
+            'author_id': author_id,
+            'author': MyUser.objects.get(user_id=author_id).nickname if author_id > 0 else "Anonymous"
         }))
+
+    def get_user_image_url(self, user):
+
+        if user.myuser.photo:
+            image = '/media/' + str(user.myuser.photo)
+        elif user.myuser.identity == "T":
+            image = '/static/image/default.png'
+        else:
+            image = '/static/image/default1.png'
+        return image
+
+    # Receive client enter from room group
+    async def client_enter(self, event):
+        await self.send(text_data=json.dumps(event))
+
+    # Receive client leave from room group
+    async def client_leave(self, event):
+        print(event)
+        await self.send(text_data=json.dumps(event))
 
     @database_sync_to_async
     def get_myuser(self):
@@ -88,11 +129,31 @@ class ChatConsumer(AsyncWebsocketConsumer):
         return self.myuser.nickname if self.user.is_authenticated else "Anonymous"
 
     @database_sync_to_async
+    def return_to_current_list_and_add_to_db(self):
+
+        s = []
+        for current_user in self.room_object.myuser_set.all():
+            d = {}
+            d['image'] = self.get_user_image_url(current_user.user)
+            d['id'] = current_user.user.id
+            d['name'] = current_user.nickname
+            s.append(d)
+
+        # add
+        self.myuser.chat_room.add(self.room_object)
+        return s
+
+    @database_sync_to_async
+    def delete_from_db(self):
+        self.myuser.chat_room.remove(self.room_object)
+
+    @database_sync_to_async
     def save_group(self):
-        ChatRoomGroup.objects.get_or_create(name=self.room_group_name)
+        return ChatRoomGroup.objects.get_or_create(name=self.room_group_name)[0]
 
     @database_sync_to_async
     def save_history(self, history):
+
         """
         save to db
         :param history:
@@ -103,26 +164,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @database_sync_to_async
     def get_history(self):
-        """
-        get history when first connect
-        :return:
-        """
+
         s = []
         for i in ChatRoomGroup.objects.get(name=self.room_group_name).chathistory_set.all():
             d = {}
-
             d["message"] = i.history
-            try:
-                if i.user.photo == "":
-                    if i.user.identity == "T":
-                        d["image"] = '/static/image/default.png'
-                    else:
-                        d["image"] = '/static/image/default1.png'
-                else:
-                    d["image"] = '/media/' + str(i.user.photo)
-            except:
-                d["image"] = '/static/image/default.png'
-
+            d['image'] = self.get_user_image_url(
+                i.user.user) if i.user else '/static/image/default.png'
+            d["author_id"] = i.user.user.id if i.user else 0
             d["isAuthor"] = "1" if self.user.is_authenticated and self.myuser == i.user else "0"
             d["time"] = i.time.strftime('%Y-%m-%d %H:%M:%S')
             d["author"] = i.user.nickname if i.user else "Anonymous"
@@ -134,13 +183,19 @@ class FriendConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.user = self.scope["user"]
-
         self.room_group_name = self.scope['url_route']['kwargs']['myuser_id']
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
         await self.accept()
+        wait_for_reply_list = RequestReplyFriend.objects.filter(reply_user_id=self.user.myuser.id)
+        for o in wait_for_reply_list:
+            await self.send(text_data=json.dumps({
+                'request_person': o.request_user.nickname,
+                'request_person_id': o.request_user.id,
+                'type': "request_add_friend",
+            }))
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -149,30 +204,37 @@ class FriendConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-    # Receive message from WebSocket
+    # Receive message from WebSocket, send it to corresponding group
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        print(text_data_json)
+        # if the user confirms becoming friends, make them friend in the database
+        request_person_id = text_data_json['request_person']
+        reply_person_id = text_data_json['reply_person']
+        if (text_data_json['type'] == "agree_add_friend"):
+            await  self.make_friends(request_person_id, reply_person_id)
+            await self.delete_request_from_db(request_person_id, reply_person_id)
+        if (text_data_json['type'] == "request_add_friend"):
+            await self.add_request_to_db(request_person_id, reply_person_id)
         await self.channel_layer.group_send(
             str(text_data_json['group']),
             text_data_json
         )
 
-    # Receive message from group
+    # Receive request_add_friend message from group
+    # Send it to the frontend of this user for confirmation
     async def request_add_friend(self, event):
         request_person = await self.get_myuser(event['request_person'])
-        print (request_person)
 
         await self.send(text_data=json.dumps({
             'request_person': request_person.nickname,
             'request_person_id': request_person.id,
             'type': "request_add_friend",
-
         }))
 
+    # Receive agree_add_friend message from group
+    # Send back to the frontend of this request user
     async def agree_add_friend(self, event):
         reply_person = await self.get_myuser(event['reply_person'])
-        print (reply_person)
         await self.send(text_data=json.dumps({
             'reply_person': reply_person.nickname,
             'reply_person_id': reply_person.id,
@@ -183,3 +245,17 @@ class FriendConsumer(AsyncWebsocketConsumer):
     @database_sync_to_async
     def get_myuser(self, id):
         return MyUser.objects.get(id=id) if self.user.is_authenticated else None
+
+    @database_sync_to_async
+    def make_friends(self, request_person_id, reply_person_id):
+        request_person = MyUser.objects.get(id=request_person_id)
+        reply_person = MyUser.objects.get(id=reply_person_id)
+        request_person.friend.add(reply_person)
+
+    @database_sync_to_async
+    def add_request_to_db(self, request_person_id, reply_person_id):
+        RequestReplyFriend(request_user_id=request_person_id, reply_user_id=reply_person_id).save()
+
+    @database_sync_to_async
+    def delete_request_from_db(self, request_person_id, reply_person_id):
+        RequestReplyFriend.objects.filter(request_user_id=request_person_id, reply_user_id=reply_person_id).delete()
