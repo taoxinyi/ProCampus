af2a415de1666c26f69d54402245f1d325dbdd60
taoxinyi <850804004@qq.com>
2018-07-01 19:43:02 +0800

    7.1 final version
    optimize UI

--- announcement/views.py
+++ announcement/views.py
@@ -8,38 +8,70 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 
 
 class AnnouncementCreateView(AjaxableResponseMixin, UserPassesTestMixin, CreateView):
+    """
+    Announcement View
+    """
     model = Announcement
     fields = ['content']
     template_name_suffix = '_create_form'
     success_url = reverse_lazy('announcement-list')
 
     def test_func(self):
+        """
+        a test function
+        :return: boolean; whether the user is staff
+        """
         return self.request.user.is_staff
 
     def get_context_data(self, **kwargs):
+        """
+        add active-page string to context
+        :param kwargs:
+        :return:context
+        """
         context = super(AnnouncementCreateView, self).get_context_data(**kwargs)
         context['active_page'] = 'announcement-add'
         return context
 
     def form_valid(self, form):
+        """
+        return form valid
+        :param form: form
+        :return:form_valid
+        """
         form.instance.author = self.request.user
         return super(AnnouncementCreateView, self).form_valid(form)
 
 
 class AnnouncementListView(UserPassesTestMixin, ListView):
+    """
+    Announcement List View
+    """
     model = Announcement
     context_object_name = 'announcement_list'
 
     def test_func(self):
+        """
+        a test function
+        :return: boolean; whether the user is staff
+        """
         return self.request.user.is_staff
 
     def get_context_data(self, **kwargs):
+        """
+        add active-page string to context
+        :param kwargs:
+        :return:context
+        """
         context = super(AnnouncementListView, self).get_context_data(**kwargs)
         context['active_page'] = 'announcement-list'
         return context
 
 
 class AnnouncementUpdateView(AjaxableResponseMixin, UserPassesTestMixin, UpdateView):
+    """
+    Announcement Update View
+    """
     model = Announcement
     context_object_name = 'announcement'
     template_name_suffix = '_update_form'
@@ -47,21 +79,43 @@ class AnnouncementUpdateView(AjaxableResponseMixin, UserPassesTestMixin, UpdateV
     fields = ['content']
 
     def test_func(self):
-        return self.request.user.is_staff
+        """
+        a test function
+        :return: boolean; whether the user is staff
+        """
 
     def get_context_data(self, **kwargs):
+        """
+        add active-page string to context
+        :param kwargs:
+        :return:context
+        """
         context = super(AnnouncementUpdateView, self).get_context_data(**kwargs)
         context['active_page'] = 'announcement-update'
         return context
 
 
 class AnnouncementDeleteView(AjaxableResponseMixin, UserPassesTestMixin, DeleteView):
+    """
+    Announcement Delete View
+    """
     model = Announcement
     success_url = reverse_lazy('announcement-list')
 
     def test_func(self):
+        """
+        a test function
+        :return: boolean; whether the user is staff
+        """
         return self.request.user.is_staff
 
     def post(self, request, *args, **kwargs):
+        """
+        after post delete
+        :param request:
+        :param args:
+        :param kwargs:
+        :return: post success status
+        """
         super(AnnouncementDeleteView, self).post(request, *args, **kwargs)
         return JsonResponse({'state': 'success'})
--- authentication/views.py
+++ authentication/views.py
@@ -17,12 +17,20 @@ from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 
 
 class SignupView(FrontMixin, CreateView):
+    """
+    The View when the user click signup
+    """
     model = MyUser
     fields = ['nickname', 'identity', 'photo']
     template_name_suffix = '_create_form'
     success_url = reverse_lazy('user-login')
 
     def form_valid(self, form):
+        """
+        When the user submit the form and it's valid.
+        :param form: the form from the frontend
+        :return: super
+        """
         username = form.data.get('username', '')
         password = form.data.get('password', '')
         email = form.data.get('email', '')
@@ -34,6 +42,11 @@ class SignupView(FrontMixin, CreateView):
         return super(SignupView, self).form_valid(form)
 
     def form_invalid(self, form):
+        """
+        When the user submit the form and it's not valid.
+        :param form: the form from the frontend
+        :return: super
+        """
         print(form.errors)
         context = {'message': form.errors, 'announcement': Announcement.objects.all()[0],
                    'category_list': Category.objects.annotate(question_num=Count('question')).order_by('name')}
@@ -41,19 +54,37 @@ class SignupView(FrontMixin, CreateView):
 
 
 class LogoutView(RedirectView):
+    """
+    The View when the user click logout
+    return to home page
+    """
     pattern_name = 'homepage'
 
     def get_redirect_url(self, *args, **kwargs):
+        """
+        return to home page
+        :param args:
+        :param kwargs:
+        :return:
+        """
         logout(self.request)
         return super(LogoutView, self).get_redirect_url(*args, **kwargs)
 
 
 class LoginView(FrontMixin, FormView):
+    """
+    The view when the user click login
+    """
     template_name = 'authentication/user_login.html'
     success_url = reverse_lazy('homepage')
     form_class = LoginForm
 
     def form_valid(self, form):
+        """
+        If the form vaild, check database first and redirect to previous page
+        :param form:
+        :return:
+        """
         user = form.login()
         if user is not None:
             if user.is_active:
@@ -72,12 +103,20 @@ class LoginView(FrontMixin, FormView):
             return self.response_error_page('用户名或密码错误')
 
     def response_error_page(self, msg):
+        """
+        return error page
+        :param msg: the error message
+        :return: rendered view of error page
+        """
         context = {'message': msg, 'announcement': Announcement.objects.all()[0],
                    'category_list': Category.objects.annotate(question_num=Count('question')).order_by('name')}
         return render(self.request, 'utils/error_page.html', context)
 
 
 class UserListView(UserPassesTestMixin, ListView):
+    """
+    The view of User list
+    """
     login_url = reverse_lazy('user-login')
     model = MyUser
     paginate_by = 30
@@ -94,6 +133,9 @@ class UserListView(UserPassesTestMixin, ListView):
 
 
 class UserPhotoChangeView(LoginRequiredMixin, FrontMixin, UpdateView):
+    """
+    The view when the user want to change photo
+    """
     login_url = reverse_lazy('user-login')
     success_url = reverse_lazy('homepage')
     template_name = 'authentication/photo_change_form.html'
@@ -101,11 +143,19 @@ class UserPhotoChangeView(LoginRequiredMixin, FrontMixin, UpdateView):
     model = MyUser
 
     def form_valid(self, form):
+        """
+        if form vaild and the id is the same, change photo for the user
+        :param form:
+        :return:
+        """
         if int(self.kwargs['pk']) != self.request.user.myuser.id:
             return self.response_error_page('权限错误')
         return super(UserPhotoChangeView, self).form_valid(form)
 
     def response_error_page(self, msg):
+        """
+        show error page
+        :param msg: error msg
+        :return:
+        """
         return render(self.request, 'utils/error_page.html', {'message': msg, 'myuser': self.request.user.myuser})
-
-
